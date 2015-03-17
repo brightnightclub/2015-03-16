1a710a14c556dda1fc3956c23bb06f75f4ef7e92
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-03-17 04:08:04 +0000

    Ready to run (maybe)

---
+++ mudrakers/mudrakers_data.rb
@@ -0,0 +1,62 @@
+# mudrakers_data.rb
+
+ARTIFACT_NAMES = [
+  "donut",
+  "cupcake",
+  "battle axe",
+  "wizard staff",
+  "hockey stick",
+  "roast beast",
+  "hand grenade",
+  "iphone 6",
+  "vise grips",
+  "parakeet cage",
+  "lasso",
+  "handgun",
+  "litterbox",
+  "fruitcake from 1974",
+  ]
+
+NPC_NAMES = [
+  "orc",
+  "troll",
+  "hazelnut princess",
+  "yeti",
+  "bus driver",
+  "spider monkey",
+  "minotaur",
+  "unicorn",
+  "shoe salesman",
+  "wizard",
+  "dragon",
+  "barbarian",
+  "sorceress",
+  "aardvark",
+  "small town drunk",
+  "marine biologist",
+  "C# dev",
+  "murderbot"
+  ]
+
+ADJECTIVES = [
+  "cuddly",
+  "fearsome",
+  "ridiculous",
+  "obstreperous",
+  "damn sexy",
+  "rabid",
+  "hypnotically beautiful",
+  "glowing",
+  "vile",
+  "wretched",
+  "enchanted",
+  "ululating",
+  "hirsute",
+  "gently cooing",
+  "ashamed",
+  "sassy",
+  "sticky",
+  "cagey",
+  "flatulent",
+  "corpulent"
+]
